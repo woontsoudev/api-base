bdfaa072dade36e6ddb1669c1d59118693144144
woontsoudev <wtsoudev@gmail.com>
2023-01-25 18:41:25 -0600

    first commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+export const start = () => {
+  try {
+    app.listen(3000, () => {
+      console.log('Server started on port 3000');
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
